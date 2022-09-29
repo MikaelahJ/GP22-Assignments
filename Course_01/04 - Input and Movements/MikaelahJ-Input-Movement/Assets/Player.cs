0249cdd4f662a445b79f43c471a7f2e05ebc5f26
MikaelahJ <mikjen0129@skola.goteborg.se>
2022-09-29 12:49:16 +0200

    Updated player

--- Course_01/04 - Input and Movements/MikaelahJ-Input-Movement/Assets/Player.cs
+++ Course_01/04 - Input and Movements/MikaelahJ-Input-Movement/Assets/Player.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,13 +6,14 @@ using UnityEngine;
 public class Player : ProcessingLite.GP21
 {
     Vector2 pos;
-    float diameter = 0.5f;
-    float maxSpeed = 20;
+    float diameter = 3f;
+    float maxSpeed = 5f;
+    [SerializeField] float deAcc = 0.25f;
 
-    float v;
-    float a = 5;
+    float vel = 2f;
+    Vector2 acc;
     Vector2 dir;
-    Vector2 zero;
+
     private void Start()
     {
         pos.x = Width / 2;
@@ -21,24 +23,63 @@ public class Player : ProcessingLite.GP21
     {
         Background(0);
 
-        v += a * Time.deltaTime;
-        if (v > maxSpeed)
+        dir += new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+
+        if (dir.magnitude > maxSpeed)
         {
-            v = maxSpeed;
+            dir = dir.normalized * maxSpeed;
         }
 
-        dir.x += v * Input.GetAxis("Horizontal") * Time.deltaTime;
-        dir.y += v * Input.GetAxis("Vertical") * Time.deltaTime;
+        acc += dir * vel * Time.deltaTime;
+
+        if (Input.anyKey == false)
+        {
+            acc *= deAcc;
+        }
+
+        pos += acc * Time.deltaTime;
+
+
+        if (pos.x > Width * 2)
+        {
+            pos.x = -Width;
+        }
+        if (pos.x < -Width)
+        {
+            pos.x = Width * 2;
+        }
+        if (pos.y > Height * 2)
+        {
+            pos.y = -Height;
+        }
+        if (pos.y < -Height)
+        {
+            pos.y = Height * 2;
+        }
+
         
-        pos += dir * Time.deltaTime;
-        pos = Vector2.Lerp(pos, zero, Time.deltaTime);
 
-        //if(pos.x > Width)
-        //{
-        //    pos.x = 0;
-        //}
+        Draw();
+    }
 
-        Stroke(255);
+    private void Draw()
+    {
+        Stroke(0, 158, 163);
+
         Circle(pos.x, pos.y, diameter);
+        Circle(pos.x + Width, pos.y, diameter);
+        Circle(pos.x - Width, pos.y, diameter);
+
+        Circle(pos.x, pos.y + Height, diameter);
+        Circle(pos.x - Width, pos.y + Height, diameter);
+        Circle(pos.x + Width, pos.y + Height, diameter);
+
+        Circle(pos.x, pos.y - Height, diameter);
+        Circle(pos.x - Width, pos.y - Height, diameter);
+        Circle(pos.x + Width, pos.y - Height, diameter);
+
+
+
+
     }
 }
